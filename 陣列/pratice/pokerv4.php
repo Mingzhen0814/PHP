d70a3e7a565608b51d733b51b8c47a2d6097259a
Mingzhen0814 <fcum1102930@gmail.com>
2024-01-26 14:14:31 +0800

    筆記

--- 陣列/pratice/pokerv4.php
+++ 陣列/pratice/pokerv4.php
@@ -24,16 +24,22 @@
 <table border="1" width="100%">
 
     <?php
-        // 3. 理牌 + 攤牌
-        $launchs = ['<font color=black>&spades;','<font color=red>&hearts;', '<font color=red>&diams;' ,'<font color=black>&clubs;'];
+        // 3. 理牌 + 攤牌 
+        // $i  | 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 ... 51
+        //-------------------------------------------------------------------------------
+        // 花色 | 0 ~ 12 黑桃 / 13 ~ 25 紅心 / 26 ~ 39 方塊 / 40 ~ 51 梅花   (int)($i/13)
+        //-------------------------------------------------------------------------------
+        // 值   | 0 ~ 12 A~K / 13 ~ 25 A~K / 26 ~ 39 A~K / 40 ~ 53 A~K      ($i%13)
+
+        $suits = ['<font color=black>&spades;','<font color=red>&hearts;', '<font color=red>&diams;' ,'<font color=black>&clubs;'];
         $values = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
         foreach($players as $player){
             echo '<tr>';
             sort($player);
             foreach($player as $card){
-                $launch = $launchs[(int)($card / 13)];
+                $suit = $suits[(int)($card / 13)];
                 $value = $values[$card % 13];
-                echo "<td>{$launch}{$value}</font></td>";
+                echo "<td>{$suit}{$value}</font></td>";
             }
             echo '</tr>';
         }   
